7c3f03e376b16f71dcbfedf8bc271f1cbe06c07f
FranciscoBS89 <francisco.spadaro@outlook.com>
2023-10-10 02:47:43 -0300

    Morgan
    Adicionado Morgan para exibir logs das requisições http no terminal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 // Configuração do middleware e do body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan('dev'))  // logs for http requests in terminal
 
 const routes = require('./Routes');
 
